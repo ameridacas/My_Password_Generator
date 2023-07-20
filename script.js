a6c9a65a232e7b236cbc8c7246c7a7367bca2a92
Alexander Merida Castillo <mcastilloalex@gmail.com>
2023-07-19 17:05:31 -0700

    Created Some Variables

---
+++ script.js
@@ -0,0 +1,29 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+function generatePassword () {
+  var myGeneratedPassword = "";
+  var uppercaseLetters ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowercaseLetters ="abcdefghijklmnopqrstuvwxyz";
+  var symbols = "~`@#$%^&*()_>-=+;:',<./?\|[]{}";
+  var numbers = "0123456789";
+  
+  var length
+  var uppercase
+  var lowercase
+  var symbols
+  var numbers
+
+  return myGeneratedPassword;
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
